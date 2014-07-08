cc235cccc8a5e845c9d93bcc0c8b29b08849f4f3
akira_cn <akira.cn@gmail.com>
2014-07-08 17:38:57 +0800

    update

--- templates/js-template-smart/src/resource.js
+++ templates/js-template-smart/src/resource.js
@@ -1,33 +1,19 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
     CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png"
 };
 
 var g_resources = [
     //image
     res.HelloWorld_png,
     res.CloseNormal_png,
-    res.CloseSelected_png,
-    'res/birds.png',
-    'res/ortho-test1.png',
-    'res/tiles/tai_1.png',
-    'res/button_music.png',
-    'res/button_sound.png',
     
     //plist
-    'res/birds.plist',
 
-    //fnt
-    'res/A Damn Mess.ttf',
-    'res/American Typewriter.ttf',
+    //fnt,
 
     //tmx
-    'res/test.tmx',
-    //'res/test.json',
 
     //bgm
-    'res/game_music.mp3',
     //effect
-    'res/ready.mp3',
 ];
